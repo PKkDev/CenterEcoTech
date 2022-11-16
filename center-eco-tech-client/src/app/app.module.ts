fe64e2c121981a250d00ed628daf1df144ad7adf
Кирилл Портнов <prodevkir@mail.ru>
2022-11-16 18:41:50 +0400

    update

--- center-eco-tech-client/src/app/app.module.ts
+++ center-eco-tech-client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // routing
 import { AppRoutingModule } from './app-routing.module';
 // modules
@@ -8,23 +9,10 @@ import { AuthorizeModule } from './modules/authorize/authorize.module';
 import { ClientAppModule } from './modules/client-app/client-app.module';
 // components
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AccrualsPageComponent } from './modules/accruals-page/accruals-page.component';
-import { IndicationsPageComponent } from './modules/indications-page/indications-page.component';
-import { ApplicationsPageComponent } from './modules/applications-page/applications-page.component';
-import { ServicesPageComponent } from './modules/services-page/services-page.component';
-import { NewsPageComponent } from './modules/news-page/news-page.component';
-import { ProfilePageComponent } from './modules/profile-page/profile-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AccrualsPageComponent,
-    IndicationsPageComponent,
-    ApplicationsPageComponent,
-    ServicesPageComponent,
-    NewsPageComponent,
-    ProfilePageComponent
+    AppComponent
   ],
   imports: [
     ClientAppModule,
