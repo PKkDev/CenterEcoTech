8f51fa21c101fad353cf5235e52fb2f9badff897
Portnov Kirill <prodevkir@mail.ru>
2022-12-10 17:02:35 +0400

    fix for test

--- center-eco-tech-client/src/app/modules/client-app/pages/profile-page/profile-page.component.spec.ts
+++ center-eco-tech-client/src/app/modules/client-app/pages/profile-page/profile-page.component.spec.ts
@@ -1,4 +1,9 @@
+import { HttpClientModule } from '@angular/common/http';
+import { StaticProvider } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from 'src/app/modules/authorize/services/auth.service';
+import { ApiService } from 'src/app/services/api.service';
 
 import { ProfilePageComponent } from './profile-page.component';
 
@@ -6,11 +11,15 @@ describe('ProfilePageComponent', () => {
   let component: ProfilePageComponent;
   let fixture: ComponentFixture<ProfilePageComponent>;
 
+  const baseAppUrl: StaticProvider = { provide: 'BASE_APP_URL', useValue: '/', deps: [] };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProfilePageComponent ]
+      imports: [HttpClientModule, ReactiveFormsModule],
+      declarations: [ProfilePageComponent],
+      providers: [AuthService, ApiService, baseAppUrl]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ProfilePageComponent);
     component = fixture.componentInstance;
