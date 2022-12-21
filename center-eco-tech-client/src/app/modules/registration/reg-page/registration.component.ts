9aa554018a27a5f02d28f66acf84e0b51b5e8cd6
janemess <hev2000@yandex.ru>
2022-12-21 05:52:34 +0400

    Communication of registration with the server

--- center-eco-tech-client/src/app/modules/registration/reg-page/registration.component.ts
+++ center-eco-tech-client/src/app/modules/registration/reg-page/registration.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 import { ApiService } from 'src/app/services/api.service';
@@ -11,21 +12,29 @@ import { CooperativeDto } from './domain';
 })
 export class RegistrationComponent implements OnInit, AfterViewInit {
 
-  constructor( private apiService: ApiService) { }
+  constructor(  
+    private apiService: ApiService, 
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
   // fields
   public phone: string;
+  public name: string;
   public coops: CooperativeDto[] = [];
+  public selectedCoop: number;
   // data
   public coopData: CooperativeDto[] = [];
   // http
   private coopDetailSubs: Subscription;
+  private userRegSups: Subscription;
+  public message: string | null;
 
 
   public onNextCLick() {
+    this.message = null;
+    this.registerUser();
   }
 
   ngAfterViewInit() {
@@ -37,7 +46,10 @@ export class RegistrationComponent implements OnInit, AfterViewInit {
       .subscribe({
         next: data => { this.coopData = data; 
         },
-        error: error => { if (this.coopDetailSubs) this.coopDetailSubs.unsubscribe(); },
+        error: error => { 
+          if (this.coopDetailSubs) this.coopDetailSubs.unsubscribe();
+          this.message = error.error;
+         },
         complete: () => { this.feetCooperatives(); }
       });
   }
@@ -49,7 +61,36 @@ export class RegistrationComponent implements OnInit, AfterViewInit {
 
   }
 
+  private registerUser() {
+    const httpBody = {
+      cooperativeId: this.selectedCoop,
+      phone: this.phone,
+      firstName: this.name
+    };
+    if (this.selectedCoop != null && this.phone != null && this.name != null) {
+      this.userRegSups = this.apiService.post<RegistrationDto>('client/register', httpBody)
+      .subscribe({
+        next: data => {   this.router.navigate(['']);  },
+        error: error => {
+          if (this.userRegSups) this.userRegSups.unsubscribe();
+          this.message = error.error;
+        },
+        complete: () => { }
+      })
+    }
+    else {
+      this.message = "Заполните все поля";
+    }
+   };
+
   public submit() {
   }
 
 }
+
+export class RegistrationDto {
+  public name: number;
+  public phone: string;
+  public firstName: string;
+}
+
