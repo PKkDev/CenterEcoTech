427cfe0a765d9284526631f4ffa4a3dbdb931ce7
janemess <hev2000@yandex.ru>
2022-12-26 08:53:42 +0400

    Indication page meters styles Adding

---
+++ center-eco-tech-client/src/app/modules/client-app/pages/indications-page/indication/domain.ts
@@ -0,0 +1,6 @@
+export class clientCounterDto {
+    public id: number;
+    public name: string;
+    public postfix: string;
+    public clientId: string;
+}
--- center-eco-tech-client/src/app/modules/client-app/pages/indications-page/indication/indication.component.ts
+++ center-eco-tech-client/src/app/modules/client-app/pages/indications-page/indication/indication.component.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { clientCounterDto } from './domain';
+import { ApiService } from 'src/app/services/api.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-indication',
@@ -8,20 +11,54 @@ import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 })
 export class IndicationComponent implements OnInit {
 
-  public meters: any = [];
+  public clientCounterDto: clientCounterDto[] = [];
   public currImg: string;
   public images: any = [];
 
+
+  private addMeterSups: Subscription;
+  public adding: boolean = false;
+  public selectedMeterName: string = "Горячая вода";
+  public message: string | null;
+
   constructor(private cdr: ChangeDetectorRef,
-    private httpClient: HttpClient){}
+    private httpClient: HttpClient,
+    private apiService: ApiService){}
 
   ngOnInit(): void {
    
-    this.httpClient.get("assets/meters.json").subscribe({
+    this.httpClient.get<clientCounterDto[]>("assets/meters.json").subscribe({
       next : data =>{
-      this.meters = data
+      this.clientCounterDto = data
       }
     });
-
     }
+
+  public activateAddingMeter(): void{
+    this.adding = true;
+  }
+
+  private addMeter() {
+    this.message = null;
+    const httpBody = {
+      name : this.selectedMeterName
+    };
+    this.addMeterSups = this.apiService.post('measurement/add-new-counter', httpBody)
+    .subscribe({
+      next: data => { },
+          error: error => { if (this.addMeterSups) this.addMeterSups.unsubscribe(); this.message = error.error; },
+          complete: () => { this.message = "Счётчик успешно добавлен" }
+    })
+  }
+
+  public onAddCLick() {
+    // this.adding = false;
+    this.addMeter();
+  }
+
+  public onReadyCLick() {
+    this.message = null;
+    this.adding = false;
+  }
+
   }
