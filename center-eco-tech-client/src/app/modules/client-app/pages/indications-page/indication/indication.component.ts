9c31078fd97a4d3f738fee8d4ef7ac9c06e44660
janemess <hev2000@yandex.ru>
2022-12-26 11:36:14 +0400

    Merge branch 'main' of https://github.com/PKkDev/CenterEcoTech into 37-indications-page-meters

--- center-eco-tech-client/src/app/modules/client-app/pages/indications-page/indication/indication.component.ts
+++ center-eco-tech-client/src/app/modules/client-app/pages/indications-page/indication/indication.component.ts
@@ -1,5 +1,15 @@
 import { HttpClient } from '@angular/common/http';
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { ApiService } from 'src/app/services/api.service';
+import { UserDetailDto } from './domain';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+interface type {
+  value: string;
+  viewValue: string;
+}
 import { clientCounterDto } from './domain';
 import { ApiService } from 'src/app/services/api.service';
 import { Subscription } from 'rxjs';
@@ -9,68 +19,93 @@ import { Subscription } from 'rxjs';
   templateUrl: './indication.component.html',
   styleUrls: ['./indication.component.scss']
 })
-export class IndicationComponent implements OnInit {
 
-  public clientCounterDto: clientCounterDto[] = [];
-  public currImg: string;
-  public images: any = [];
+export class IndicationComponent implements OnInit, AfterViewInit, OnDestroy {
 
+  myDate: Date;
+  public indicationForm: FormGroup;
+  // data
+  public userDetaill: UserDetailDto;
+  public name: string | null = null;
+  public pastvalue: string | null = null;
+  public currentvalue: string | null = null;
+  public selectedType: string;
 
-  private addMeterSups: Subscription;
-  private lastMeasureSups: Subscription;
-  public adding: boolean = false;
-  public selectedMeterName: string = "Горячая вода";
-  public message: string | null;
+  // http
+  private userDetailSubs: Subscription;
+  private updateDetailSubs: Subscription;
+  // private updateDetailSubs: Subscription;
+  // private ConfirmUserSubs: Subscription;
+  private addRequestsSubs: Subscription;
 
-  constructor(private cdr: ChangeDetectorRef,
-    private httpClient: HttpClient,
-    private apiService: ApiService){}
+  constructor(
+    private snackBar: MatSnackBar,
+    private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.getUserDetail();
+  }
+
+  allType: type[] = [
+    { value: '1', viewValue: 'Горячая вода' },
+    { value: '2', viewValue: 'Холодная вода' },
+    { value: '3', viewValue: 'Газ' },
+  ]
+
+  ngAfterViewInit() {
+    this.getUserDetail();
+  }
+
+  ngOnDestroy() {
+    if (this.addRequestsSubs) this.addRequestsSubs.unsubscribe();
+
+    // if (this.userDetailSubs) this.userDetailSubs.unsubscribe();
+    // if (this.updateDetailSubs) this.updateDetailSubs.unsubscribe();
+    // if (this.ConfirmUserSubs) this.ConfirmUserSubs.unsubscribe();
+  }
+
+  private getUserDetail() {
+    this.userDetailSubs = this.apiService.get<UserDetailDto>('client/detail')
+      .subscribe({
+        next: data => { this.userDetaill = data; },
+        error: error => { if (this.userDetailSubs) this.userDetailSubs.unsubscribe(); },
+        complete: () => { }
+      });
+  }
+
+  public formEnable() {
+    this.indicationForm.enable();
+  }
+
+  public formDisable() {
+    this.indicationForm.disable();
+  }
+
+  public onCreateRequestClick() {
+    debugger;
+    if (!this.name || !this.name) return;
+    this.addRequestsSubs = this.apiService.post('measurement/add-measurement', this.indicationForm.value)
+      .subscribe({
+        next: data => {
+          this.snackBar.open('успешно', 'OK');
+          this.name = null;
+          this.currentvalue = null;
+        },
+        error: error => {
+          if (this.addRequestsSubs) this.addRequestsSubs.unsubscribe();
+        },
+        complete: () => { }
+      });
+  }
+
+  public checkFields() {
+    const check = !this.name || !this.name;
+    return check;
+  }
+
+  constructor() { }
 
   ngOnInit(): void {
-  //  this.getLastMeasurement();
-    this.httpClient.get<clientCounterDto[]>("assets/meters.json").subscribe({
-      next : data =>{
-      this.clientCounterDto = data
-      }
-    });
-    }
-
-  public activateAddingMeter(): void{
-    this.adding = true;
-  }
-
-  private getLastMeasurement() {
-    this.lastMeasureSups = this.apiService.post('measurement/get-last-measurement')
-    .subscribe({
-      next: data => { 
-        console.log(data);
-      },
-          error: error => { if (this.lastMeasureSups) this.lastMeasureSups.unsubscribe(); this.message = error.error; },
-          complete: () => { }
-    })
-  }
-
-  private addMeter() {
-    this.message = null;
-    const httpBody = {
-      name : this.selectedMeterName
-    };
-    this.addMeterSups = this.apiService.post('measurement/add-new-counter', httpBody)
-    .subscribe({
-      next: data => { },
-          error: error => { if (this.addMeterSups) this.addMeterSups.unsubscribe(); this.message = error.error; },
-          complete: () => { this.message = "Счётчик успешно добавлен" }
-    })
-  }
-
-  public onAddCLick() {
-    // this.adding = false;
-    this.addMeter();
-  }
-
-  public onReadyCLick() {
-    this.message = null;
-    this.adding = false;
   }
 
   }
--- center-eco-tech-client/src/app/modules/client-app/pages/news-page/news-page.component.ts
+++ center-eco-tech-client/src/app/modules/client-app/pages/news-page/news-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef, Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-news-page',
@@ -7,9 +9,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsPageComponent implements OnInit {
 
-  constructor() { }
+   // template
+   @ViewChild('allNewsTemplate') allNewsTemplate: TemplateRef<any>;
+   @ViewChild('articleTemplate') articleTemplate: TemplateRef<any>;
+   public nowTemplate: TemplateRef<any>;
+   public articles: any = [];
+   public currArticle: any = [];
 
-  ngOnInit(): void {
+  constructor(private cdr: ChangeDetectorRef,
+    private httpClient: HttpClient){}
+
+  ngOnInit(){
+    this.httpClient.get("assets/articles/articles.json").subscribe({
+      next : data =>{
+      this.articles = data;
+      }
+    })
+  }
+
+  ngAfterViewInit() {
+    this.onLoadTemplate();
+  }
+
+  public onLoadTemplate() {
+    switch (this.currArticle.length) {
+      case (0): {
+        this.nowTemplate = this.allNewsTemplate; 
+        break;
+      }
+      case (1): { 
+        this.nowTemplate = this.articleTemplate; 
+        break;
+    }
+      default: this.nowTemplate = this.allNewsTemplate; break;
+    }
+    this.cdr.detectChanges();
+  }
+
+  public setArticle(currPic: string, currName:string, currContent:string) {
+    this.currArticle.push({
+      "pic" : currPic,
+      "name" : currName,
+      "content" : currContent
+    });
+    console.log(this.currArticle);
+    this.onLoadTemplate();
+  }
+
+  onBackClick() {
+    this.currArticle = [];
+    this.nowTemplate = this.allNewsTemplate;
   }
 
 }
